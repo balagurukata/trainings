2f22d22ec0178c2a4faed135e4c6427227696c3d
bala <gnagurus@gmail.com>
2020-02-03 23:00:13 +0530

    Added - Test-case for divisible by 3

--- fizzbuzz/src/main/java/com/training/kata/FizzBuzz.java
+++ fizzbuzz/src/main/java/com/training/kata/FizzBuzz.java
@@ -1,5 +1,12 @@
 package com.training.kata;
 
 public class FizzBuzz {
+	
+	private static final String STR_FIZZ= "fizz";
+
+	public String divisible(int number) {
+		
+		return STR_FIZZ;
+	}
 
 }
--- fizzbuzz/src/test/java/com/training/kata/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/com/training/kata/FizzBuzzTest.java
@@ -1,14 +1,24 @@
 package com.training.kata;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
+import org.junit.Test;
 
 public class FizzBuzzTest {
 	
 	private FizzBuzz fizzBuzz;
 	
+	private static final String STR_FIZZ= "fizz";
+	
 	@Before
 	public void init() {
 		fizzBuzz = new FizzBuzz();
+	}
+	
+	@Test
+	public void testShouldReturnFizzIfNumberDivisibleBy3() {
+		assertEquals(STR_FIZZ, fizzBuzz.divisible(3));
 	}
 
 }
