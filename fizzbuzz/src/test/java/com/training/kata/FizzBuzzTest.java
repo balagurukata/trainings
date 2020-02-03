73c377f5f5f9e94cddc8a36dc5275fc31f7fe487
bala <gnagurus@gmail.com>
2020-02-03 22:49:41 +0530

    Updated - FizzBuzzTest.java with init method. Created - FizzBuzz.java, based on the testcase class dependency.

--- fizzbuzz/src/test/java/com/training/kata/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/com/training/kata/FizzBuzzTest.java
@@ -1,5 +1,14 @@
 package com.training.kata;
 
+import org.junit.Before;
+
 public class FizzBuzzTest {
+	
+	private FizzBuzz fizzBuzz;
+	
+	@Before
+	public void init() {
+		fizzBuzz = new FizzBuzz();
+	}
 
 }
