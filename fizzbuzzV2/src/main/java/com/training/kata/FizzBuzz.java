41ac0ec52f21bfd32994d327fe552c808a9f9565
bala <test@gmail.com>
2020-02-07 11:33:00 +0530

    Added: Test case should return the fizz string, if given number is divisible by 3.

--- fizzbuzzV2/src/main/java/com/training/kata/FizzBuzz.java
+++ fizzbuzzV2/src/main/java/com/training/kata/FizzBuzz.java
@@ -2,4 +2,9 @@ package com.training.kata;
 
 public class FizzBuzz {
 
+	public String divisible(int i) {
+		// TODO Auto-generated method stub
+		return "fizz";
+	}
+
 }
--- fizzbuzzV2/src/test/java/com/training/kata/FizzBuzzTest.java
+++ fizzbuzzV2/src/test/java/com/training/kata/FizzBuzzTest.java
@@ -1,6 +1,7 @@
 package com.training.kata;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class FizzBuzzTest {
@@ -9,6 +10,12 @@ public class FizzBuzzTest {
 	public void newFizzBuzzInstanceShouldNotBeNull(){
 		FizzBuzz fizzbuzz = new FizzBuzz();
 		assertNotNull(fizzbuzz);
+	}
+	
+	@Test
+	public void shouldReturnFizzIfGivenNumberDivisiableBy3(){
+		FizzBuzz fizzbuzz = new FizzBuzz();
+		assertEquals("fizz", fizzbuzz.divisible(3));
 	}
 
 }
